39baa3c1f95cb9516264bbc0fb687b1cc911f1de
Nagy Zétény <nagyzeteny6@gmail.com>
2023-04-29 19:13:56 +0200

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,42 +1,38 @@
 import Link from 'next/link';
 
-const computeVariantClasses = (variant?: string) => {
+type Variant = 'red' | 'green' | 'blue';
+
+const computeClasses = (variant: Variant) => {
+  const base =
+    'cursor-pointer whitespace-nowrap rounded border-2 px-4 py-2 text-sm font-bold text-white transition-all hover:bg-transparent';
+
   switch (variant) {
     case 'red':
-      return 'border-red-500 bg-red-500 hover:text-red-500';
+      return `${base} border-red-500 bg-red-500 hover:text-red-500`;
     case 'green':
-      return 'border-green-500 bg-green-500 hover:text-green-500';
-    default:
-      return 'border-blue-700 bg-blue-700 hover:text-blue-700';
+      return `${base} border-green-500 bg-green-500 hover:text-green-500`;
+    case 'blue':
+      return `${base} border-blue-700 bg-blue-700 hover:text-blue-700`;
   }
 };
 
 export const Button: React.FC<{
   onClick?: () => void;
   href?: string;
-  variant?: 'blue' | 'red' | 'green';
+  variant?: Variant;
   type?: 'button' | 'submit' | 'reset';
   children: React.ReactNode;
-}> = ({ onClick, href, variant, type = 'button', children }) => {
+}> = ({ onClick, href, variant = 'blue', type = 'button', children }) => {
   if (href) {
     return (
-      <Link
-        className={`whitespace-nowrap rounded border-2 px-4 py-2 text-sm font-bold text-white transition-all hover:bg-transparent
-        ${computeVariantClasses(variant)}`}
-        href={href}
-      >
+      <Link className={computeClasses(variant)} href={href}>
         {children}
       </Link>
     );
   }
 
   return (
-    <button
-      type={type}
-      className={`whitespace-nowrap rounded border-2 px-4 py-2 text-sm font-bold text-white transition-all hover:bg-transparent
-      ${computeVariantClasses(variant)}`}
-      onClick={onClick}
-    >
+    <button type={type} className={computeClasses(variant)} onClick={onClick}>
       {children}
     </button>
   );
