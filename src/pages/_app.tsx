c5dc486652f6088f4817f8557dedad5598fd7d1d
Nagy Zétény <nagyzeteny6@gmail.com>
2023-04-29 12:47:21 +0200

    Vercel Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { Session } from 'next-auth';
 import type { AppType } from 'next/dist/shared/lib/utils';
 import { SessionProvider } from 'next-auth/react';
+import { Analytics } from '@vercel/analytics/react';
 import { trpc } from '@utils/trpc';
 import { Navigation } from '@components/Navigation';
 
@@ -10,6 +11,7 @@ const App: AppType<{
   session: Session | null;
 }> = ({ Component, pageProps: { session, ...pageProps } }) => (
   <SessionProvider session={session}>
+    <Analytics />
     <Navigation />
     <Component {...pageProps} />
   </SessionProvider>
