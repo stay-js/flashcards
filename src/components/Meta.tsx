bdcd16a093106de31f063f8460f5a916a62e7038
Nagy Zétény <nagyzeteny6@gmail.com>
2023-04-30 15:08:50 +0200

    latest packages, Update Meta.tsx

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -6,18 +6,22 @@ export const Meta: React.FC<{
   desc: string;
 }> = ({ path, title, desc }) => (
   <Head>
+    <meta charSet="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 
     <meta name="author" content="Zétény Nagy" />
+
     <meta
       name="keywords"
       content="flashcards, flashcards app, stay, znagy, znagy.hu, quizlet, github"
     />
 
+    <meta name="theme-color" content="#1d4ed8" />
+
     <meta property="og:locale" content="en_US" />
-    <meta property="og:type" content="website" key="og_type" />
-    <meta property="og:site_name" content="Flashcards" key="site_name" />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="Flashcards" />
 
     <meta name="twitter:card" content="summary_large_image" />
     <meta property="twitter:domain" content="flashcards.znagy.hu" />
@@ -27,7 +31,7 @@ export const Meta: React.FC<{
     <meta name="rating" content="general" />
 
     <title>{title}</title>
-    <meta property="og:title" content={title} key="title" />
+    <meta property="og:title" content={title} />
     <meta name="twitter:title" content={title} />
 
     <meta name="description" content={desc} />
