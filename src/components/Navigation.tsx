2a414bb76e5d20b5bd29e6b0d02e5d815411d59c
Nagy Zétény <nagyzeteny6@gmail.com>
2023-05-10 17:17:31 +0200

    latest packages, eslint, navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -8,33 +8,35 @@ export const Navigation: React.FC = () => {
   const { data: session } = useSession();
 
   return (
-    <div className="relative flex min-h-[5rem] items-center justify-between bg-white px-6 py-4 shadow-sm">
-      {useRouter().asPath === '/' && session ? (
-        <div className="flex items-center gap-4">
-          {session.user?.image && (
-            <Image
-              className="select-none rounded-full"
-              src={session.user.image}
-              alt="GitHub Profile Picture"
-              width={48}
-              height={48}
-            />
-          )}
-          <p className="flex flex-col text-lg sm:flex-row sm:gap-1">
-            Hi, <span className="font-bold">{session.user?.name}!</span>
-          </p>
-        </div>
-      ) : (
-        <Link href="/" className="text-xl font-bold">
-          Flashcards
-        </Link>
-      )}
+    <header>
+      <nav className="relative flex min-h-[5rem] items-center justify-between bg-white px-6 py-4 shadow-sm">
+        {useRouter().asPath === '/' && session ? (
+          <div className="flex items-center gap-4">
+            {session.user?.image && (
+              <Image
+                className="select-none rounded-full"
+                src={session.user.image}
+                alt="GitHub Profile Picture"
+                width={48}
+                height={48}
+              />
+            )}
+            <p className="flex flex-col text-lg sm:flex-row sm:gap-1">
+              Hi, <span className="font-bold">{session.user?.name}!</span>
+            </p>
+          </div>
+        ) : (
+          <Link href="/" className="text-xl font-bold">
+            Flashcards
+          </Link>
+        )}
 
-      {session ? (
-        <Button onClick={() => void signOut()}>Sign Out</Button>
-      ) : (
-        <Button onClick={() => void signIn('github')}>Sign In</Button>
-      )}
-    </div>
+        {session ? (
+          <Button onClick={() => signOut()}>Sign Out</Button>
+        ) : (
+          <Button onClick={() => signIn('github')}>Sign In</Button>
+        )}
+      </nav>
+    </header>
   );
 };
