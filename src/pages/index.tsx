1f05ec121ee2ce07a02e556292290276bc0c3b37
Nagy Zétény <nagyzeteny6@gmail.com>
2023-05-25 18:41:34 +0200

    search no sets state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,41 +57,46 @@ const Sets: React.FC = () => {
 
       {isLoading && <LoadingSpinner />}
 
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-        {sets?.map(({ id, name, description, _count: { cards }, visibility, category }) => (
-          <Link
-            href={`/sets/${encodeURIComponent(id)}`}
-            key={id}
-            className="flex h-52 flex-col gap-3 overflow-hidden rounded-lg border bg-white p-4 shadow-sm"
-          >
-            <div className="flex items-center justify-between">
-              <div>
-                <h2 className="line-clamp-1 text-lg font-bold">{name}</h2>
-                <h3 className="line-clamp-2">{description}</h3>
-              </div>
+      {!isLoading &&
+        (sets.length ? (
+          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+            {sets.map(({ id, name, description, _count: { cards }, visibility, category }) => (
+              <Link
+                href={`/sets/${encodeURIComponent(id)}`}
+                key={id}
+                className="flex h-52 flex-col gap-3 overflow-hidden rounded-lg border bg-white p-4 shadow-sm"
+              >
+                <div className="flex items-center justify-between">
+                  <div>
+                    <h2 className="line-clamp-1 text-lg font-bold">{name}</h2>
+                    <h3 className="line-clamp-2">{description}</h3>
+                  </div>
 
-              <FaExternalLinkAlt
-                className="text-transparent transition-all duration-200 group-hover:text-black"
-                size={20}
-              />
-            </div>
+                  <FaExternalLinkAlt
+                    className="text-transparent transition-all duration-200 group-hover:text-black"
+                    size={20}
+                  />
+                </div>
 
-            <div className="flex gap-2">
-              <span className="flex w-fit items-center rounded-full bg-gray-200 px-3 py-1.5 ">
-                {visibility === 'PRIVATE' ? <TbLock /> : <TbWorld />}
-              </span>
+                <div className="flex gap-2">
+                  <span className="flex w-fit items-center rounded-full bg-gray-200 px-3 py-1.5 ">
+                    {visibility === 'PRIVATE' ? <TbLock /> : <TbWorld />}
+                  </span>
 
-              <span className="flex w-fit rounded-full bg-gray-200 px-3 py-1.5 text-xs font-medium text-gray-800">
-                {category}
-              </span>
+                  <span className="flex w-fit rounded-full bg-gray-200 px-3 py-1.5 text-xs font-medium text-gray-800">
+                    {category}
+                  </span>
 
-              <span className="flex w-fit rounded-full bg-gray-200 px-3 py-1.5 text-xs font-medium text-gray-800">
-                {cards} cards
-              </span>
-            </div>
-          </Link>
+                  <span className="flex w-fit rounded-full bg-gray-200 px-3 py-1.5 text-xs font-medium text-gray-800">
+                    {cards} cards
+                  </span>
+                </div>
+              </Link>
+            ))}
+          </div>
+        ) : (
+          <span className="text-center font-bold">There are no sets to display...</span>
         ))}
-      </div>
     </main>
   );
 };
