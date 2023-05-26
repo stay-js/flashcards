4a7beeabd1b174ed614e317dbcac51bd2d033099
Nagy Zétény <nagyzeteny6@gmail.com>
2023-05-26 17:39:39 +0200

    Update pagination-button.tsx

--- src/components/pagination-button.tsx
+++ src/components/pagination-button.tsx
@@ -4,7 +4,7 @@ export const PaginationButton: React.FC<{
   onClick: () => void;
 }> = ({ direction, disabled, onClick }) => (
   <button
-    className="select-none rounded-lg bg-gray-200 px-4 py-2 hover:bg-gray-300 disabled:cursor-default disabled:bg-gray-200 disabled:opacity-50"
+    className="select-none rounded-lg bg-gray-200 px-4 py-2 hover:bg-gray-300 disabled:cursor-not-allowed disabled:bg-gray-200 disabled:opacity-50"
     disabled={disabled}
     onClick={onClick}
   >
